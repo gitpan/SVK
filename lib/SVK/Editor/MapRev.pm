284698770c1aab93dda6e750a74984c7013a27ab
Chia-liang Kao (高嘉良) <clkao@clkao.org>
2006-12-13 11:39:44 +0000

    Import of CLKAO/SVK-1.99_05 from CPAN.
    gitpan-cpan-distribution: SVK
    gitpan-cpan-version:      1.99_05
    gitpan-cpan-path:         CLKAO/SVK-1.99_05.tar.gz
    gitpan-cpan-author:       CLKAO
    gitpan-cpan-maturity:     developer

--- lib/SVK/Editor/MapRev.pm
+++ lib/SVK/Editor/MapRev.pm
@@ -48,73 +48,36 @@
 # and any derivatives thereof.
 # 
 # END BPS TAGGED BLOCK }}}
-package SVK::Root;
+package SVK::Editor::MapRev;
 use strict;
 use warnings;
 
-use base qw{ Class::Accessor::Fast };
+use base 'SVK::Editor::ByPass';
 
-__PACKAGE__->mk_accessors(qw(root txn pool cleanup_pid));
+__PACKAGE__->mk_accessors(qw(cb_resolve_rev));
+
+sub rev_at {
+    my ($self, $method) = @_;
+    return 0 if $method eq 'open_root';
+    return 1 if $method eq 'delete_entry';
+    return 2 if $method =~ m'^open_';
+    return 3 if $method =~ m'^add_';
+    return -1;
+}
 
 sub AUTOLOAD {
+    my ($self, @arg) = @_;
     my $func = our $AUTOLOAD;
     $func =~ s/^.*:://;
     return if $func =~ m/^[A-Z]+$/;
 
-    no strict 'refs';
-    no warnings 'redefine';
-
-    *$func = sub {
-        my $self = shift;
-        my $path = shift;
-        $path = $path->stringify if index(ref($path), 'Path::Class') == 0;
-        # warn "===> $self $func: ".join(',',@_).' '.join(',', (caller(0))[0..3])."\n";
-        unshift @_, $path if defined $path;
-        return $self->root->$func(@_);
-    };
-
-    goto &$func;
-}
-
-sub new {
-    my $class = shift;
-    my $self = $class->SUPER::new(@_);
-    $self->{createdby} = join(' ', (caller(2))[0..2]);
-    return $self;
-}
-
-sub DESTROY {
-    return unless $_[0]->txn;
-    # if this destructor is called upon the pool cleanup which holds the
-    # txn also, we need to use a new pool, otherwise it segfaults for
-    # doing allocation in a pool that is being destroyed.
-    $_[0]->txn->abort( SVN::Pool->new )
-        if $_[0]->txn && ( $_[0]->cleanup_pid || $$ ) == $$;
-}
-
-# return the root and path on the given revnum, the returned path is
-# with necessary translations.
-sub get_revision_root {
-    my $self = shift;
-    my $path = shift;
-    return ( $self->new({root => $self->fs->revision_root(@_)}), 
-	     $path );
-}
-
-sub txn_root {
-    my ($self, $pool) = @_;
-    my $txn = $self->fs->begin_txn($self->revision_root_revision, $pool);
-    return $self->new({ txn => $txn, root => $txn->root($pool), cleanup_pid => $$ });
-}
-
-sub same_root {
-    my ($self, $other) = @_;
-    return 1 if $self eq $other;
-    return unless ref($self) eq __PACKAGE__ && ref($other) eq __PACKAGE__;
-    if ($self->txn) {
-	return $other->txn ? $self->txn->name eq $other->txn->name : 0;
+    my $rev = $self->rev_at($func);
+    unless ($rev == -1) {
+	$arg[$rev] = $self->cb_resolve_rev->($func, $arg[$rev]);
     }
-    return $self->revision_root_revision == $other->revision_root_revision;
+
+    $func = "SUPER::$func";
+    $self->$func(@arg);
 }
 
 1;
